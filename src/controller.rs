37d10c26d414fb8311d316a874f16a61bd8eeb2c
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-09-13 13:09:22 +0800

    Add todo

--- src/controller.rs
+++ src/controller.rs
@@ -1,2 +1,15 @@
 // My thought is that these would mainly be
 // modifications on the model
+
+// Move: (index, moveTo)
+
+// TODO:
+// check_move
+// check_win
+// make_move
+// list_all_moves
+// list_piece_moves
+//
+// possibly also handle history:
+// state_count
+// goto_state
