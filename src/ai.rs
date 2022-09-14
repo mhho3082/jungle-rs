e87ad3e1bb2fa9b8e638704e455efcffee71c83a
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-09-14 13:47:35 +0800

    Add aggressive naive AI
    Now it is very hard to win against the AI
    (at least for me)...

--- src/ai.rs
+++ src/ai.rs
@@ -9,7 +9,7 @@ pub fn _ai_random(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
     *list_all_moves(state).choose(rng).unwrap()
 }
 
-pub fn _ai_naive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
+pub fn _ai_naive_defensive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
     let all_moves = list_all_moves(state);
 
     // Get in den already!
@@ -35,7 +35,64 @@ pub fn _ai_naive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
             return **attack_moves.choose(rng).unwrap();
         } else {
             // Just randomly pick one
+            // This means that the AI doesn't really try to move towards enemy's den
             return *all_moves.choose(rng).unwrap();
+        }
+    }
+}
+
+pub fn _ai_naive_aggressive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
+    let all_moves = list_all_moves(state);
+
+    // Get in den already!
+    let win_moves: Vec<&(i32, i32)> = all_moves
+        .iter()
+        .filter(|(_, y)| {
+            if state.cur_blue {
+                y == &DEN_RED
+            } else {
+                y == &DEN_BLUE
+            }
+        })
+        .collect();
+    if !win_moves.is_empty() {
+        return **win_moves.choose(rng).unwrap();
+    } else {
+        // Capture if possible
+        let attack_moves: Vec<&(i32, i32)> = all_moves
+            .iter()
+            .filter(|(_, y)| find_capture(state, *y))
+            .collect();
+        if !attack_moves.is_empty() {
+            return **attack_moves.choose(rng).unwrap();
+        } else if state.cur_blue {
+            // Find farthest move (blue)
+            let mut farthest: i32 = 63;
+            for (_, y) in &all_moves {
+                if y < &farthest {
+                    farthest = *y;
+                }
+            }
+            return **all_moves
+                .iter()
+                .filter(|(_, y)| y == &farthest)
+                .collect::<Vec<&(i32, i32)>>()
+                .choose(rng)
+                .unwrap();
+        } else {
+            // Find farthest move (red)
+            let mut farthest: i32 = 0;
+            for (_, y) in &all_moves {
+                if y > &farthest {
+                    farthest = *y;
+                }
+            }
+            return **all_moves
+                .iter()
+                .filter(|(_, y)| y == &farthest)
+                .collect::<Vec<&(i32, i32)>>()
+                .choose(rng)
+                .unwrap();
         }
     }
 }
