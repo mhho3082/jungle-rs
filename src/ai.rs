aeb53a23a7f2bc312d64a33b0a54c544469f8321
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-09-14 13:28:49 +0800

    Add comments

--- src/ai.rs
+++ src/ai.rs
@@ -11,6 +11,8 @@ pub fn _ai_random(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
 
 pub fn _ai_naive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
     let all_moves = list_all_moves(state);
+
+    // Get in den already!
     let win_moves: Vec<&(i32, i32)> = all_moves
         .iter()
         .filter(|(_, y)| {
@@ -24,6 +26,7 @@ pub fn _ai_naive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
     if !win_moves.is_empty() {
         return **win_moves.choose(rng).unwrap();
     } else {
+        // Capture if possible
         let attack_moves: Vec<&(i32, i32)> = all_moves
             .iter()
             .filter(|(_, y)| find_capture(state, *y))
@@ -31,6 +34,7 @@ pub fn _ai_naive(state: &State, rng: &mut ThreadRng) -> (i32, i32) {
         if !attack_moves.is_empty() {
             return **attack_moves.choose(rng).unwrap();
         } else {
+            // Just randomly pick one
             return *all_moves.choose(rng).unwrap();
         }
     }
